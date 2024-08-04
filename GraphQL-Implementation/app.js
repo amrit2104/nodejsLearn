6f342be3cbe273c666fe59d09192c1ef323f926b
amrit2104 <amrit2104@gmail.com>
2024-08-05 02:09:01 +0530

    basic app.js created

--- GraphQL-Implementation/app.js
+++ GraphQL-Implementation/app.js
@@ -1,34 +1,13 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const graphqlHttp = require('express-graphql');
-const { buildSchema } = require('graphql'); // we can build our schema as a string.
 
-const app = express(); //express app-object
+const app = express();
 
 app.use(bodyParser.json());
 
-//localhost:3030 will print Hello World!
-// app.get('/', (req,res,next) => {
-//     res.send('Hello World!');
-// })
-
-app.use('/graphql', graphqlHttp({
-    // backticks(``) this allows you to write multiline string in javascript.
-    schema: buildSchema(`
-        type RootQuery { 
-
-        }
-
-        type RootMutation { 
-
-        }
-
-        schema {
-            query:
-            mutation: 
-        }
-    `),
-    rootValue: {}
-}));
+// get request is getting routed to Hello World!
+app.get('/', (req,res,next) => {
+    res.send('Hello World!');
+})
 
 app.listen(3030);
