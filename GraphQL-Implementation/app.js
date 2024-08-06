e8bf95e84d52fc912f3ac2110bd7e7e497ab80f3
amrit2104 <amrit2104@gmail.com>
2024-08-06 17:27:29 +0530

    changes to fetch an event

--- GraphQL-Implementation/app.js
+++ GraphQL-Implementation/app.js
@@ -6,6 +6,8 @@ const { buildSchema } = require('graphql'); //this will generate a graphql schem
 
 const app = express();
 
+const events = [];
+
 app.use(bodyParser.json());
 
 // get request is getting routed to Hello World!
@@ -23,13 +25,31 @@ app.use(
     // here RootQuery is an object where we are going to define real endpoints.
     // In graphql world, you always define list of something.
     // [String!]!: this wont return null or a list of null strings
+    // ! means arrays non-nullable
+    // here Event is a type or you can think of a class like TreeNode*
+    // you don't need comma(',') is graphql, you can simply start a new line
     schema: buildSchema(`
+        type Event {
+            _id: ID! 
+            title: String!
+            description: String!
+            price: Float!
+            date: String!
+        }
+
+        input EventInput {
+            title: String!
+            description: String!
+            price: Float!
+            date: String!
+        }
+
         type RootQuery { 
-            events: [String!]!
+            events: [Event!]!
         }
 
         type RootMutation {
-            createEvent(name: String): String
+            createEvent(eventInput: EventInput): Event
         }
 
         schema {
@@ -40,11 +60,20 @@ app.use(
     // the below rootValue thing is a resolver
     rootValue: {
         events: () => {
-            return ['Romantic Cooking', 'Sailing', 'All-Night Coding']
+            // return ['Romantic Cooking', 'Sailing', 'All-Night Coding']
+            return events;
         },
         createEvent: (args) => {
-            const eventName = args.name;
-            return eventName;
+            const event = {
+                _id: Math.random().toString(),
+                title: args.eventInput.title, //it will now directly fetch from the arguments passed
+                description: args.eventInput.description, //we are eventInput because that is where we are passing the argument
+                price: +args.eventInput.price, // + converts the string to a number
+                date: args.eventInput.date
+            }
+            events.push(event);
+            // const eventName = args.name;
+            return event;
         }
     },
     // the below statement will redirect to an URL where you gonna get nice user interface
