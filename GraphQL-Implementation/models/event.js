bd61c5806e46d140d9ca5404431716180d3c044e
amrit2104 <amrit2104@gmail.com>
2024-08-08 21:29:10 +0530

    6. changes user and event creation

---
+++ GraphQL-Implementation/models/event.js
@@ -0,0 +1,29 @@
+// a javascript model created with mongoose package
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const eventSchema = new Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    price: {
+        type:Number,
+        required: true
+    },
+    date: {
+        type: Date,
+        required: true
+    },
+    creator: {
+        type: Schema.Types.ObjectId,
+        ref: 'User'
+    }
+});
+// ref and this below line will establise communication between Event and Users.
+module.exports = mongoose.model('Event', eventSchema);
